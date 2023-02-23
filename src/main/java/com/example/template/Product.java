811aedbfdeccd536c71df1f2052d81aa8233c76e
Jangwon Lee <saraitne11@naver.com>
2023-02-23 15:41:42 +0900

    Update Product.java

--- src/main/java/com/example/template/Product.java
+++ src/main/java/com/example/template/Product.java
@@ -11,7 +11,7 @@ import java.util.List;
 @JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class)
 public class Product {
     
-    ## SKCC - AWS CodeBuild Test
+    // SKCC - AWS CodeBuild Test
     
     @Id
     @GeneratedValue
